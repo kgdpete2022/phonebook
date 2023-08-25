6cb1620f52991ce284c5cbb1b10a941b34686f25
kgdpete2022 <mail4dev2022@gmail.com>
2023-08-25 14:15:09 +0200

    Updated several functions in phonebook.py

--- phonebook.py
+++ phonebook.py
@@ -11,46 +11,66 @@ HEADER_FIELDS = [
 ]
 
 
-def get_sorted_list_from_csv(CSV_FILE):
-    """Takes a csv file and returns a sorted list of lists"""
+def read_list_from_csv():
+    """Takes a csv file and returns a list of contacts as lists"""
     with open(CSV_FILE, "r") as f:
-        csv_f = csv.reader(f)
-        contacts = []
-        for contact in csv_f:
-            contacts.append(contact)
-    header = contacts[0]
-    contacts = contacts[1:]
-    contacts.sort()
-    return header + contacts
+        # Convert data to list omitting the header
+        contacts = list(csv.reader(f))[1:]
+    return contacts
 
 
-def write_sorted_list_to_csv(CSV_FILE, contacts):
-    """Takes a sorted list of lists and writes it into a csv file"""
+def write_list_to_csv(contacts):
+    """Takes a list of contacts, adds header fields, writes the result into a csv file."""
     with open(CSV_FILE, "w") as f:
         write = csv.writer(f)
+        write.writerow(HEADER_FIELDS)
         write.writerows(contacts)
         print("The file {CSV_FILE} has been updated sucessfully")
 
 
-def print_contacts(contacts, contacts_per_page=None):
+def add_contact_to_phonebook(contact):
+    """Reads contacts from csv, adds new contact, writes the updated contacts to CSV"""
+    contacts = read_list_from_csv()
+    contacts.append(contact)
+    return contacts
+
+
+def print_phonebook(contacts_per_page=None):
+    contacts = read_list_from_csv()
+    contacts.sort()
     total_contacts = len(contacts)
     if contacts_per_page == None:
         contacts_per_page = total_contacts
     total_full_pages = total_contacts // contacts_per_page
-    contacts_on_last_page = total_contacts % contacts_per_page
 
-    for i in range(len(contacts)):
+    # number of contacts on the last page if total contacts not evenly divided by contacts per page
+    contacts_on_partly_filled_page = total_contacts % contacts_per_page
+
+    for i in range(total_contacts):
+        # prints header fiels on every new page of contacts
+        if (i + 1) % contacts_per_page == 1:
+            print("-" * 130)
+            print(
+                "{:<15} {:<15} {:<15} {:<45} {:<20} {:<20}".format(
+                    *HEADER_FIELDS
+                )
+            )
+            print("-" * 130)
+
         print("{:<15} {:<15} {:<15} {:<45} {:<20} {:<20}".format(*contacts[i]))
+
+        # prints a line and contacts range at the end of each page of contacts
         if (i + 1) % contacts_per_page == 0:
             print("-" * 130)
             print(
                 f"contacts {i + 2 - contacts_per_page}-{i + 1} of {total_contacts}"
             )
-    if contacts_on_last_page:
+    # prints a line and contacts range on the last page of contacts (if total contacts not evenly divided by contacts per page)
+    if contacts_on_partly_filled_page:
         print("-" * 130)
         print(
             f"contacts {total_full_pages * contacts_per_page + 1}-{total_contacts} of {total_contacts}"
         )
 
 
-print_contacts(get_sorted_list_from_csv(CSV_FILE), 20)
+print_phonebook(20)
